a797d458e4756e7a735016ebbc07be8787c35bed
Disyam Adityana <disyam.adityana@gmail.com>
2016-11-03 14:47:32 +0700

    add challenge power of two

--- test/world.prime.factors/prime.factors.spec.js
+++ test/world.prime.factors/prime.factors.spec.js
@@ -5,16 +5,16 @@ var primeFactorsOf = require('../../libs/world.prime.factors/lib/prime.factors.o
 
 describe('Prime factors decomposition', function() {
 
-	it('can decompose 2', function() {
-		expect(primeFactorsOf(2)).to.equal([2]);
-	});
-	
-	it('can decompose 8', function() {
-		expect(primeFactorsOf(8)).to.equal([2, 2, 2]);
-	});
-	
-	it('can decompose 27', function() {
-		expect(primeFactorsOf(27)).to.equal([3, 3, 3]);
-	});
+  it('can decompose 2', function() {
+    expect(primeFactorsOf(2)).to.deep.equal([2]);
+  });
+
+  it('can decompose 8', function() {
+    expect(primeFactorsOf(8)).to.deep.equal([2, 2, 2]);
+  });
+
+  // it('can decompose 27', function() {
+  // 	expect(primeFactorsOf(27)).to.equal([3, 3, 3]);
+  // });
 
 });
