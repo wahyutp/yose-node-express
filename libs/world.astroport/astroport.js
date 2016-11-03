d5891c5f8fde7d0e92540e4a871c47340d3708c8
byusuf <b.yusuf.w@gmail.com>
2016-11-03 14:43:43 +0700

    add astroport name

--- libs/world.astroport/astroport.js
+++ libs/world.astroport/astroport.js
@@ -1,6 +1,6 @@
 var astroport = function(request, response) {
     response.setHeader('Content-Type', 'text/html');
-    response.render('astroport');
+    response.render('astroport.html');
 };
 
 module.exports = astroport;
