867bbc5ffd1682c70e8d36dd85a3cc409b0a5ceb
byusuf <b.yusuf.w@gmail.com>
2016-11-04 14:47:57 +0700

    update Astroport Gates test

--- test/astroport.spec.js
+++ test/astroport.spec.js
@@ -1,7 +1,9 @@
 var request = require('request');
 var http    = require('http');
+var Browser = require('zombie');
 var chai = require('chai');
 var expect = chai.expect;
+var browser = new Browser();
 var server  = require('../libs/server');
 
 describe('Passing the astroport name test:', function() {
@@ -16,6 +18,29 @@ describe('Passing the astroport name test:', function() {
         testServer.close();
     });
 
+    /* Astroport Test */
+    it("include a#contact-me-link", function(done){
+        browser.visit("http://localhost:7000", function(err) {
+            browser.assert.element('#contact-me-link');
+
+            done();
+        });
+    });
+
+    /* Gates Test */
+    it("include #gate and #ship", function(done){
+        browser.visit("http://localhost:7000/astroport", function(err) {
+            browser.assert.element('#gate-1');
+            browser.assert.element('#gate-2');
+            browser.assert.element('#gate-3');
+            browser.assert.element('#gate-1 #ship-1');
+            browser.assert.element('#gate-2 #ship-2');
+            browser.assert.element('#gate-3 #ship-3');
+
+            done();
+        });
+    });
+
     it('answers with text/html header', function(done) {
         request('http://localhost:7000/astroport', function(error, response, body) {
             expect(response.headers['content-type']).to.equal('text/html');
@@ -26,6 +51,26 @@ describe('Passing the astroport name test:', function() {
     it('returns the expected output', function(done) {
         request('http://localhost:7000/astroport', function(error, response, body) {
             expect(body).to.contain( 'Astroport' );
+            done();
+        });
+    });
+
+    /* Docker */
+    it("include #ship and #dock", function(done){
+        browser.visit("http://localhost:7000/astroport", function(err) {
+            browser.assert.element('#ship');
+            browser.assert.element('#dock');
+
+            done();
+        });
+    });
+
+    it("should display ship1 name on #ship1 after #dock clicked", function(done){
+        var opts = {url: 'http://localhost:7000/astroport', method: 'post'};
+        request(opts, function (err, res, body) {
+            // you will need to customize the assertions below based on your server
+            // expect(body).to.contain( 'Astroport' );
+
             done();
         });
     });
